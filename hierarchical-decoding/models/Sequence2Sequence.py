7c73a28f3d75513f6358f939bba5a70e9192a10e
Marco A. S. Cabezudo <msobrevillac@gmail.com>
2020-10-10 01:40:25 -0300

    fixing attention

--- hierarchical-decoding/models/Sequence2Sequence.py
+++ hierarchical-decoding/models/Sequence2Sequence.py
@@ -1,6 +1,6 @@
 from layers.Decoder import Decoder
 from layers.Encoder import Encoder
-from layers.Attention import Attention
+from layers.Attention import BahdanauAttention
 import torch
 import torch.nn as nn
 
