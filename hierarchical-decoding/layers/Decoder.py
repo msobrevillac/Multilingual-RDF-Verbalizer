65eaf10372ca002c3b293d86fb8d14fc6369b24b
Marco A. S. Cabezudo <msobrevillac@gmail.com>
2020-10-10 18:42:23 -0300

    adding dropout and layer normalization on embeddings

--- hierarchical-decoding/layers/Decoder.py
+++ hierarchical-decoding/layers/Decoder.py
@@ -153,7 +153,7 @@ class Generator(nn.Module):
     """Define standard linear + softmax generation step."""
     def __init__(self, hidden_size, vocab_size):
         super(Generator, self).__init__()
-        self.proj = nn.Linear(hidden_size, vocab_size, bias=False)
+        self.proj = nn.Linear(hidden_size, vocab_size, bias=True)
 
     def forward(self, x):
         return F.log_softmax(self.proj(x), dim=-1)
