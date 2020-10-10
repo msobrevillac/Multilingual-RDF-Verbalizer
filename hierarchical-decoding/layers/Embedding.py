93f28b3446974d1587d6a60f327a6cb51432c398
Marco A. S. Cabezudo <msobrevillac@gmail.com>
2020-10-10 17:56:05 -0300

    adding dropout and layer normalization on embeddings

---
+++ hierarchical-decoding/layers/Embedding.py
@@ -0,0 +1,24 @@
+import torch
+import torch.nn as nn
+
+
+
+class Embedding(nn.Module):
+    """A conditional RNN decoder with attention."""
+    
+    def __init__(self, input_size, emb_size, dropout=0.0, norm=False):
+        super(DecoderRNN, self).__init__()
+        self.embedding = nn.Embedding(input_size, emb_size)
+
+        self.dropout = nn.Dropout(dropout)
+
+        self.layer_norm = nn.LayerNorm(emb_size)
+        self.norm = norm
+
+
+    def forward(self, x):
+        x = self.dropout(self.embedding(x))
+        if self.norm:
+            x = self.layer_norm(x)
+
+        return x
